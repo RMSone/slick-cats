733fa82e59ac0d2e53cfb0506231a058d16ab530
Yosef Fertel <yosef.fertel@rms.com>
2016-12-03 12:25:52 -0500

    Add Cogen and eitherT eq evidence to test

--- src/test/scala/com/rms/miu/slickcats/DBIOInstancesTest.scala
+++ src/test/scala/com/rms/miu/slickcats/DBIOInstancesTest.scala
@@ -1,13 +1,13 @@
 package com.rms.miu.slickcats
 
 import cats.data.EitherT
-import cats.laws.discipline._
 import cats.implicits._
 import cats.instances.AllInstances
 import cats.laws.discipline.CartesianTests.Isomorphisms
+import cats.laws.discipline._
 import cats.{Comonad, Eq}
 import org.scalacheck.Arbitrary.arbitrary
-import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.{Arbitrary, Cogen, Gen}
 import org.scalatest.{FunSuite, Matchers}
 import org.typelevel.discipline.scalatest.Discipline
 import slick.dbio.DBIO
@@ -21,7 +21,7 @@ class DBIOInstancesTest extends FunSuite with Matchers with Discipline with AllI
   private val timeout = 3.seconds
   private val db = slick.memory.MemoryDriver.backend.Database(global)
 
-  def dbioXor[A](f: DBIO[A]): DBIO[Either[Throwable, A]] =
+  def dbioEither[A](f: DBIO[A]): DBIO[Either[Throwable, A]] =
     f.map(Right[Throwable, A]).asTry.map {
       case Success(x) => x
       case Failure(t) => Left(t)
@@ -30,10 +30,20 @@ class DBIOInstancesTest extends FunSuite with Matchers with Discipline with AllI
   implicit def eqfa[A: Eq]: Eq[DBIO[A]] =
     new Eq[DBIO[A]] {
       def eqv(fx: DBIO[A], fy: DBIO[A]): Boolean = {
-        val fz = dbioXor(fx) zip dbioXor(fy)
+        val fz = dbioEither(fx) zip dbioEither(fy)
         Await.result(db.run(fz.map { case (tx, ty) => tx === ty }), timeout)
       }
     }
+
+  // Implicit resolution has a hard time with DBIO[A] vs DBIOAction[A, NoStream, All] nested in EitherT etc.
+  implicit def eqEitherTDbio[A: Eq]: Eq[EitherT[DBIO, Throwable, A]] = {
+    new Eq[EitherT[DBIO, Throwable, A]] {
+      override def eqv(x: EitherT[DBIO, Throwable, A], y: EitherT[DBIO, Throwable, A]): Boolean = {
+        val fz = x.value zip y.value
+        Await.result(db.run(fz.map { case (tx, ty) => tx === ty }), timeout)
+      }
+    }
+  }
 
   implicit def arbDBIO[T](implicit a: Arbitrary[T]): Arbitrary[DBIO[T]] =
     Arbitrary(Gen.oneOf(arbitrary[T].map(DBIO.successful), arbitrary[Throwable].map(DBIO.failed)))
@@ -46,20 +56,8 @@ class DBIOInstancesTest extends FunSuite with Matchers with Discipline with AllI
   implicit val nonFatalArbitrary: Arbitrary[Throwable] =
     Arbitrary(arbitrary[Exception].map(identity))
 
-  /*
-  For reasons that are not clear to me, the implicit resolution fails unless `Eq` instance for `XorT[DBOI]`
-  (EqXorTFEA: Eq[XorT[F, E, A]]) is typed explicitly. Among the errors that come out
-  are:
-    [error]  found   : cats.Eq[slick.dbio.DBIO[Throwable]]
-    [error]     (which expands to)  algebra.Eq[slick.dbio.DBIOAction[Throwable,slick.dbio.NoStream,slick.dbio.Effect.All]]
-    [error]  required: algebra.Eq[cats.data.XorT[[+R]slick.dbio.DBIOAction[R,slick.dbio.NoStream,slick.dbio.Effect.All],Throwable,Int]]
-  and:
-    diverging implicit expansion for type org.scalacheck.Arbitrary[T]
-      [error] starting with value arbInt in trait ArbitraryLowPriority
-      [error]     implicitly))
-  See gitter discussion that might be relevant: https://gitter.im/non/cats?at=564798f18b242470793db879
- */
-  implicit val eqXorTDbio: Eq[EitherT[DBIO, Throwable, Int]] = implicitly[Eq[EitherT[DBIO, Throwable, Int]]]
+  implicit def cogenForDbio[A]: Cogen[DBIO[A]] =
+    Cogen[Unit].contramap(_ => ())
 
   checkAll("DBIO[Int]", MonadErrorTests[DBIO, Throwable].monadError[Int, Int, Int])
   checkAll("DBIO[Int]", ComonadTests[DBIO].comonad[Int, Int, Int])
